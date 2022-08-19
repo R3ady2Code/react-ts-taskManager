ed284d219669f5f544f3d3b9efb656f8c7279441
T G <ghalumyan@icloud.com>
2022-08-19 16:07:09 +0300

    add redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
+import store, { persistor } from './redux/store';
 import App from './App';
-
 import './styles.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,44 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { taskReducer } from './slices/task.slice';
 import { boxReducer } from './slices/box.slice';
 import { subtaskReducer } from './slices/subtask.slice';
 
-export const store = configureStore({
-  reducer: {
-    tasks: taskReducer,
-    boxes: boxReducer,
-    subtasks: subtaskReducer,
-  },
+const rootReducer = combineReducers({
+  tasks: taskReducer,
+  boxes: boxReducer,
+  subtasks: subtaskReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type TypeRootState = ReturnType<typeof store.getState>;
+
+export const persistor = persistStore(store);
+export default store;
